bd8ee1a6aeee1a2da71f7f2c44caa842056ac83a
Ryan Ferry <ryanferry@microsoft.com>
2023-01-06 17:36:10 -0500

    Move App.py

--- src/akabackend/__init__.py
+++ src/akabackend/__init__.py
@@ -3,6 +3,12 @@
 import sys
 import os
 sys.path.insert(0, os.getcwd())
+from app import create_app
 
 
 # __version__ = "3.2"
+
+if __name__ == '__main__':
+    app = create_app()
+    # app.run(host='0.0.0.0', port=8000)
+    app.run()

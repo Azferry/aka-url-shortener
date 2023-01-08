0bed8664b1da865a0ea434446f8c7407bfaee045
Ryan Ferry <ryanferry@microsoft.com>
2023-01-08 16:08:18 -0500

    Root App

--- src/akabackend/app/__init__.py
+++ src/akabackend/app/__init__.py
@@ -55,20 +55,6 @@ def create_app(config_class=Config):
 
     middleware.exporter.add_telemetry_processor(callback_function)
 
-    @app.route('/heartbeat')
-    def heart_beat():
-        return 'Hello World, Heart Beat - Root App'
-
-    @app.route('/robots933456.txt')
-    def appsrv_catch():
-        """appsrv_catch is a dummy URL path that App Service uses to check if the container is serving requests.
-        Catches the response to stop exceptions in application insights
-        https://github.com/MicrosoftDocs/azure-docs/blob/main/includes/app-service-web-configure-robots933456.md
-        Returns:
-            string: robots933456.txt
-        """
-        return 'robots933456.txt'
-
     import app.api as api
     app.logger.info("Initializing API Blueprints")
     api.init_app(app, version=__version__, title=Config.APP_TITLE)
---
+++ src/akabackend/app/api/v1/Root.py
@@ -0,0 +1,25 @@
+from flask import abort, Blueprint, jsonify, request
+from app.common.UrlOperations import UrlOperations
+from app.common.metrics import mmap, short_url_cr_measure,vanity_url_cr_measure, tmap
+import logging
+log = logging.getLogger('app')
+app_name = __name__.split(".")[-1]
+app = Blueprint(app_name, app_name)
+
+@app.route('/heartbeat', methods=['GET'])
+def heart_beat():
+    return 'Hello World, Heart Beat - Root App'
+
+@app.route('/robots933456.txt', methods=['GET'])
+def appsrv_catch():
+    """appsrv_catch is a dummy URL path that App Service uses to check if the container is serving requests.
+    Catches the response to stop exceptions in application insights
+    https://github.com/MicrosoftDocs/azure-docs/blob/main/includes/app-service-web-configure-robots933456.md
+    Returns:
+        string: robots933456.txt
+    """
+    return 'robots933456.txt'
+
+@app.route('/', methods=['GET'])
+def root():
+    return "RootApp"
